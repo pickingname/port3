e4b26a9d5844a99ec78e0764f471f56ee1c7d4fe
pickingname <trintrinir@icloud.com>
2024-08-02 21:13:15 +0700

    feat: add images and fixed padding

--- app/reusables/footer.tsx
+++ app/reusables/footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
+    <footer className="p-5 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
       <div className="grow text-left">
         9af470a9f
       </div>
