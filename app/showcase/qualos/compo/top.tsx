3f72f6f901a4c7169cb1ca0ae1535d74c1065e15
pickingname <trintrinir@icloud.com>
2024-08-04 13:47:39 +0700

    feat: add features list

--- app/showcase/qualos/compo/top.tsx
+++ app/showcase/qualos/compo/top.tsx
@@ -7,14 +7,13 @@ export default function Intro() {
   return (
     <div className="pl-5">
       <div className="h-32 overflow-hidden mb-8 rounded-lg">
-        <Image
-          src={"https://pickingname.github.io/images/showcase/sfqr/ql-ac-li.png"}
-          alt="showcase image"
-          width={1920}
-          height={1080}
-          className="max-w-full object-cover h-full w-full rounded-lg blur-[3px]"
-        />
+        <iframe
+          src="https://minimal.qualos.info/"
+          title="preview"
+          className="max-w-full object-cover h-full w-full rounded-lg"
+        ></iframe>
       </div>
+
       <div className="flex h-5 items-center space-x-3 text-sm">
         <SquareActivity />
         <Separator orientation="vertical" />
@@ -24,6 +23,42 @@ export default function Intro() {
         <p className="text-xl pt-2">
           beautifully presented earthquake information, on the web
         </p>
+      </div>
+      <div>
+        <div className="my-8 border rounded-md">
+          <div className="flex flex-wrap -mx-2">
+            <div className="w-full md:w-1/3 px-2 mb-4">
+              <div className="rounded-lg p-6 text-center">
+                <div className="text-xl mb-2">01</div>
+                <p className="text-lg mb-2">its fast</p>
+                <p className="text-neutral-500">
+                  this website is designed to load quickly and provide you with
+                  the information
+                </p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/3 px-2 mb-4">
+              <div className="rounded-lg p-6 text-center">
+                <div className="text-xl mb-2">02</div>
+                <p className="text-lg mb-2">its straightforward</p>
+                <p className="text-neutral-500">
+                  no need to navigate through a maze of information to see a
+                  report
+                </p>
+              </div>
+            </div>
+            <div className="w-full md:w-1/3 px-2 mb-4">
+              <div className="rounded-lg p-6 text-center">
+                <div className="text-xl mb-2">03</div>
+                <p className="text-lg mb-2">its open source</p>
+                <p className="text-neutral-500">
+                  the source code for this website is available on github to
+                  ensure transparency
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
