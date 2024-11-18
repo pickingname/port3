82c4bd7ca635e12f78bf109ca1453a00783ab7fe
pickingname <trintrinir@icloud.com>
2024-11-18 12:17:17 +0000

    Pending changes exported from your codespace

--- app/pages/entry.tsx
+++ app/pages/entry.tsx
@@ -6,9 +6,15 @@ import Footer from "../reusables/gitfooter";
 const App = () => {
   return (
     <main className="py-5">
-      <div className=""> 
+      <div className="">
         <div className="max-w-4xl mx-auto pb-12">
-        <Nav />
+          <Nav />
+          <div className="text-xl pl-5">
+            <p>blogs</p>
+            <p className="font-mono text-neutral-500 text-sm">
+              16/11/24 | git420
+            </p>
+          </div>
           <Li />
           <Footer />
         </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,26 +1,23 @@
 import React from "react";
-import { Hammer, Undo2 } from "lucide-react";
+import Nav from "../entrycompo/Nav";
+import Footer from "../reusables/gitfooter";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="text-center p-6 rounded-lg shadow-lg">
-        <div className="mb-4">
-          <Hammer className="w-12 h-12 text-gray-500 mx-auto" />
+    <main className="py-5">
+      <div className="">
+        <div className="max-w-4xl mx-auto pb-12">
+          <Nav />
+          <div className="pl-5">
+            <p className="text-xl">projects directory</p>
+            <div className="pt-3">
+              naming scheme
+              <p>project list that its name contains the letter "A" is about an app, in both browser wise and </p>
+            </div>
+          </div>
+          <Footer />
         </div>
-        <h1 className="text-2xl mb-2">work in progress</h1>
-        <p className="text-gray-600 mb-4">
-          <span className="font-mono">/projects</span> directory is still under
-          development
-        </p>
-        <a
-          href="/"
-          className="inline-flex items-center px-4 py-2 bg-neutral-700 text-white rounded hover:bg-neutral-800 ease-out duration-200"
-        >
-          <Undo2 className="w-5 h-5 mr-2" />
-          go back
-        </a>
       </div>
-    </div>
+    </main>
   );
 }
