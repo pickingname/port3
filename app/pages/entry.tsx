bd642bcc34382add0d21dad7f581c8ae42eb1969
pickingname <trintrinir@icloud.com>
2024-11-18 21:06:49 +0700

    feat: add projects table

--- app/pages/entry.tsx
+++ app/pages/entry.tsx
@@ -9,6 +9,12 @@ const App = () => {
       <div className=""> 
         <div className="max-w-4xl mx-auto pb-12">
         <Nav />
+        <div className="text-xl pl-5">
+            <p>blogs/list</p>
+            <p className="font-mono text-neutral-500 text-sm">
+              click on the entries to view the full blog
+            </p>
+          </div>
           <Li />
           <Footer />
         </div>
